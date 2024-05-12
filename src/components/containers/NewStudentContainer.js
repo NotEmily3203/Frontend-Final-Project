4ab2e377e476b5345d8cbbaeeafbc30a006faad2
Will160714 <95445527+Will160714@users.noreply.github.com>
2024-05-12 14:33:59 -0400

    Added in the additional properties for student

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -20,6 +20,7 @@ class NewStudentContainer extends Component {
     this.state = {
       firstname: "", 
       lastname: "", 
+      imageURL: "",
       campusId: null, 
       redirect: false, 
       redirectId: null
@@ -40,7 +41,10 @@ class NewStudentContainer extends Component {
     let student = {
         firstname: this.state.firstname,
         lastname: this.state.lastname,
-        campusId: this.state.campusId
+        campusId: this.state.campusId,
+        email: this.state.email,
+        gpa: this.state.gpa,
+        imageURL: this.state.imageUrl,
     };
     
     // Add new student in back-end database
@@ -50,10 +54,14 @@ class NewStudentContainer extends Component {
     this.setState({
       firstname: "", 
       lastname: "", 
+      email: "",
+      imageURL: "",
+      gpa: null,
       campusId: null, 
       redirect: true, 
       redirectId: newStudent.id
     });
+
   }
 
   // Unmount when the component is being removed from the DOM:
