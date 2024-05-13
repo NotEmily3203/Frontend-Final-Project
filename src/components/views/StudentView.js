ae52cda980b8405236f8caf1f4f92929a9507ee3
Will160714 <95445527+Will160714@users.noreply.github.com>
2024-05-13 00:22:40 -0400

    Fixed display for campus name

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,8 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -11,7 +13,14 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <h3>{student.campusId}</h3>
+
+      <div>
+        <Link to={`/edit-student`}>
+          <button>Edit</button>
+        </Link>
+      </div>
+
     </div>
   );
 
