426864e44e37a6bc83b9f7930977f54ffac83dc8
Will160714 <95445527+Will160714@users.noreply.github.com>
2024-05-17 19:54:08 -0400

    Made the fields that can't be null repuired

--- src/components/views/EditCampusView.js
+++ src/components/views/EditCampusView.js
@@ -56,12 +56,12 @@ const EditCampusView = (props) => {
           </div>
           <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
             <label style= {{color:'#11153e', fontWeight: 'bold'}}>Name: </label>
-            <input type="text" name="name" defaultValue = {campus.name} onChange ={(e) => handleChange(e)} />
+            <input type="text" name="name" defaultValue = {campus.name} onChange ={(e) => handleChange(e)} required/>
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}>Address: </label>
-            <input type="text" name="address" defaultValue = {campus.address} onChange={(e) => handleChange(e)} />
+            <input type="text" name="address" defaultValue = {campus.address} onChange={(e) => handleChange(e)} required/>
             <br/>
             <br/>
 
